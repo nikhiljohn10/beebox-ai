8fed49ea118d33c90140c9514b3200a4100e03c4
Nikhil John <nikhiljohn1010@gmail.com>
2025-03-25 20:37:29 +0530

    Added auto file creation and retry

--- beebox_ai/operators.py
+++ beebox_ai/operators.py
@@ -12,29 +12,40 @@ class BEEBOXAI_OT_send_message(bpy.types.Operator):
 
     async def stream(self, context, prompt):
         error = ""
+        retry = 3
         file = get_textfile(context)
-
         try:
+            while file is None:
+                bpy.ops.text.new()
+                file = get_textfile(context)
+                retry -= 1
+                if retry == 0:
+                    raise RuntimeError("Unable to create a new text file")
+
+            # if file.as_string() != "":
+            #     input_text = "\n" + input_text
+
             client = get_openai_client()
-            if file is not None:
-                try:
-                    await client.stream(bpy.data.texts[file.name].write, prompt)
-                except api.APIConnectionError as e:
-                    error = f"The OpenAI server could not be reached: {e.__cause__}"
-                except api.AuthenticationError as e:
-                    error = (
-                        "Your not allow to access this resource. \n"
-                        "There might be an issue with your API key. \n"
-                        "Please see preferences."
-                    )
-                except api.RateLimitError as e:
-                    error = "Your OpenAI quota has been exceeded"
-                except api.APIStatusError as e:
-                    error = f"Error found in the API response from OpenAI.\nSTATUS = {e.status_code} | ERROR = {e.response}"
-                except api.PermissionDeniedError as e:
-                    error = f"Permission Error: {e}"
+            try:
+                await client.stream(bpy.data.texts[file.name].write, prompt)
+            except api.APIConnectionError as e:
+                error = f"The OpenAI server could not be reached: {e.__cause__}"
+            except api.AuthenticationError as e:
+                error = (
+                    "Your not allow to access this resource. \n"
+                    "There might be an issue with your API key. \n"
+                    "Please see preferences."
+                )
+            except api.RateLimitError as e:
+                error = "Your OpenAI quota has been exceeded"
+            except api.APIStatusError as e:
+                error = f"Error found in the API response from OpenAI.\nSTATUS = {e.status_code} | ERROR = {e.response}"
+            except api.PermissionDeniedError as e:
+                error = f"Permission Error: {e}"
         except AttributeError as e:
             error = f"API Input Error: {e}"
+        except RuntimeError as e:
+            error = f"Runtime Error: {e}"
         except Exception as e:
             error = f"Error in stream: {e}"
 
@@ -45,13 +56,6 @@ class BEEBOXAI_OT_send_message(bpy.types.Operator):
         input_text = context.scene.ai_text_input
         if input_text == "":
             return {"CANCELLED"}
-
-        file = get_textfile(context)
-        if file is None:
-            return {"CANCELLED"}
-
-        if file.as_string() != "":
-            input_text = "\n" + input_text
 
         task = asyncio.ensure_future(self.stream(context, input_text))
         try:
